06d6954e780759e6c32d49d3b10f0b49c5650928
João Otávio <joaotavioos@outlook.com>
2023-06-03 20:49:51 -0300

    feat: add express middlewares ✨

---
+++ src/infrastructure/api/express.ts
@@ -0,0 +1,51 @@
+// @ts-ignore
+import xss from "xss-clean";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import hpp from "hpp";
+import cors from "cors";
+import express, { Express } from "express";
+import { errorLogger, errorResponder } from "./middlewares/error.handlers";
+import {
+  clientsRoute,
+  productsRoute,
+  invoiceRoute,
+  checkouteRoute,
+} from "./routes";
+
+import "../database/sequelize.postgres";
+
+export const app: Express = express();
+
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+
+// Segurança aos Header Http
+app.use(helmet());
+
+//Data sanitization against XSS
+app.use(xss());
+
+//Prevent parameter pollution
+app.use(hpp());
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message:
+    "Há muitas requisições para o mesmo IP, por favor tente novamente daqui uma hora!",
+});
+
+// rate limit
+app.use("/", limiter);
+
+// CORS
+app.use(cors());
+
+app.use("/products", productsRoute);
+app.use("/clients", clientsRoute);
+app.use("/invoice", invoiceRoute);
+app.use("/checkout", checkouteRoute);
+
+app.use(errorLogger);
+app.use(errorResponder);
