73e34bf885f1f6e01972f4928ec2c1728d464a4b
João Otávio <joaotavioos@outlook.com>
2023-06-03 20:24:08 -0300

    feat: add product order foreignkey ✨

--- src/modules/store-catalog/repository/product.model.ts
+++ src/modules/store-catalog/repository/product.model.ts
@@ -1,4 +1,11 @@
-import { Column, Model, PrimaryKey, Table } from "sequelize-typescript";
+import {
+  Column,
+  ForeignKey,
+  Model,
+  PrimaryKey,
+  Table,
+} from "sequelize-typescript";
+import { CheckoutModel } from "../../checkout/repository/checkout.model";
 
 @Table({
   tableName: "products",
@@ -17,4 +24,7 @@ export default class ProductModel extends Model {
 
   @Column({ allowNull: false })
   declare salesPrice: number;
+
+  @ForeignKey(() => CheckoutModel)
+  declare ordersId: string;
 }
