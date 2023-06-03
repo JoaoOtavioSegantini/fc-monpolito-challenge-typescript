6b81fa4a17a81da7365e676f9f12e0e3d46f58ec
João Otávio <joaotavioos@outlook.com>
2023-06-03 20:52:15 -0300

    feat: export routes ✨

---
+++ src/infrastructure/api/routes/index.ts
@@ -0,0 +1,4 @@
+export { checkouteRoute } from "./checkoute.route";
+export { clientsRoute } from "./clients.route";
+export { invoiceRoute } from "./invoice.route";
+export { productsRoute } from "./products.route";
